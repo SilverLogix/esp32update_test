4d931b5c615cdd342a1cf69897e3e2aeddd47ec5
SilverLogix <cameron.c.ait@gmail.com>
2023-06-08 06:41:08 -0500

    add esp-now
    see. above

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import random
 import board
 import vga8
+import now
 import globals as g
 from sys import modules
 
@@ -15,7 +16,7 @@ if __name__ == "__main__":
     oled.set_backlight_intensity(64)
 
     oled.gwifi()
-    board.STA('NETGEAR90', 'curlyearth685')
+    board.STA('NETGEAR', 'passwword')
     time.sleep_ms(1000)
     gn = board.get_npt()
     g.ntp = gn
@@ -32,6 +33,9 @@ if __name__ == "__main__":
     oled.fillroundrect(16, 16, 90, 90, 16, oled.BLUE)
     oled.fillroundrect(128, 16, 90, 90, 16, random.choice(rand))
     oled.tft.text(vga8, f"{g.ntp[1]}/{g.ntp[2]}/{g.ntp[0]}", 86, 114, oled.WHITE, 0)
+
+    now.get_mac()
+    now.start_rx_thread()
 
     print("\n", modules.keys(), "\n")
 
--- now.py
+++ now.py
@@ -1,23 +1,85 @@
-# ESP-NOW tests
-
-
 import espnow  # noqa
-import time
+import _thread
+import network
+import ubinascii
+from time import sleep_ms
 
-
-e = espnow.ESPNow()     # Enable ESP-NOW
+e = espnow.ESPNow()  # Enable ESP-NOW
 e.active(True)
+is_rx_thread_running = False
 
-peer1 = b'\xe8\x68\xe7\x4e\xbb\x19'   # MAC address of peer1's wifi interface
-e.add_peer(peer1)                     # add peer1 (receiver1)
 
-peer2 = b'\x60\x01\x94\x5a\x9c\xf0'   # MAC address of peer2's wifi interface
+def auth():
+    # A WLAN interface must be active to send()/recv()
+    sta = network.WLAN(network.STA_IF)
+    sta.active(True)
+    sta.disconnect()                # Disconnect from last connected WiFi SSID
 
-print("Starting...")            # Send to all peers
 
-e.send(peer1, "walk", True)     # send commands to pear 1
-e.send(peer2, "walk", True)     # send commands to pear 2
-time.sleep_ms(2000)
-e.send(peer1, "walk", True)
-e.send(peer2, "back", True)
-time.sleep_ms(2000)
+def get_mac():
+    mac = ubinascii.hexlify(network.WLAN(1).config('mac'), ':').decode()
+    print(f"MAC: {mac}")
+    wlan_mac = network.WLAN(1).config('mac')
+    print(ubinascii.hexlify(wlan_mac).decode())
+
+
+def tx(mac, data):
+    global e
+    e.add_peer(mac)  # Add the receiver's MAC address
+    e.send(mac, data, True)  # Send data to the receiver
+    print(f"Sent data to : {mac}")
+    sleep_ms(1000)
+
+
+def rx():
+    global is_rx_thread_running, e
+
+    while is_rx_thread_running:
+        # Add MAIN peers mac
+        peer = b'\x84\xcc\xa8\x61\x0a\x21'  # MAC address of peer's wifi interface
+        e.add_peer(peer)  # Sender's MAC registration
+
+        while True:
+            host, msg = e.recv()
+            if msg:  # wait for message
+                if msg == b'test':  # decode message and translate
+                    print("OMG Got message!\n")
+                elif msg == b'back':
+                    pass
+                elif msg == b'stop':
+                    pass
+
+
+def start_rx_thread():
+    global is_rx_thread_running
+
+    if not is_rx_thread_running:
+        is_rx_thread_running = True
+        _thread.stack_size(128)
+        _thread.start_new_thread(rx, ())
+        print("NOW Stack:", _thread.stack_size())
+        print("Rx thread started.")
+    else:
+        print("Rx thread is already running.")
+
+
+def stop_rx_thread():
+    global is_rx_thread_running
+    if is_rx_thread_running:
+        is_rx_thread_running = False
+        print("Rx thread stopped.")
+    else:
+        print("Rx thread is not running.")
+
+
+# Usage:
+# Start the thread
+# start_rx_thread()
+
+# Send data via ESP-NOW
+# mac_address = b'\xe8\x68\xe7\x4e\xbb\x19'  # MAC address of the receiver
+# data = b'walk'  # Data to be sent
+# tx(mac_address, data)
+
+# Stop the thread
+# stop_rx_thread()
